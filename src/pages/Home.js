58634f0f767ccccee7bb8ad17b9547b66cb135d5
BaqirSalim <101154594+BaqirSalim@users.noreply.github.com>
2023-03-03 18:33:23 -0600

    home page complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
 import '../styles/Home.css';
+import Computer from '../assets/computer-vector.png';
+import Keyboard from '../assets/keyboard-vector.png';
 
 function Home()
 {
     return (
-        <div className='homeScreen'>
-            <div className='orgName'>
-                <h1>cougar</h1>
-                <h1>ai</h1>
+        <div className='parent'>
+            <div className='child keyboard'>
+                <img src={Keyboard} alt="keyboard" className='image'/>
+            </div>
+            <div className='child org-name'>
+                <h1>COUGAR</h1>
+                <h1>AI</h1>
+            </div>
+            <div className='child computer'>
+                <img src={Computer} alt="computer" className='image'/>
             </div>
         </div>
     );
